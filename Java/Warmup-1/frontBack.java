c6211130f20c1c93f45a7633a9b9f69ee21b2cc7
michaelsong93 <ss7277@seas.upenn.edu>
2020-10-08 21:56:38 +0900

    switching front and back, 100820

--- Java/Warmup-1/frontBack.java
+++ Java/Warmup-1/frontBack.java
@@ -1,11 +1,16 @@
+/*
+  Given a string, return a new string where the first and last chars have been exchanged.
+
+  frontBack("code") → "eodc"
+  frontBack("a") → "a"
+  frontBack("ab") → "ba"
+ */
+
 public String frontBack(String str) {
-    if(str.length() < 2){
-      return str;
-    }
-    else{
-      String constant = str.substring(1, str.length()-1);
-      return str.charAt(str.length()-1) + constant + str.charAt(0);
-      }
-    }
+  if(str.length() <= 1) return str;
+  String front = str.substring(0,1);
+  String mid = str.substring(1,str.length()-1);
+  String back = str.substring(str.length()-1,str.length());
   
-  
+  return back+mid+front;
+}
