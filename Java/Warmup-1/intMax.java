41e07940897958f6d7b9dd5f90c05c202636e052
michaelsong93 <ss7277@seas.upenn.edu>
2020-11-23 22:48:10 +0900

    max, 112320

---
+++ Java/Warmup-1/intMax.java
@@ -0,0 +1,25 @@
+// Given three int values, a b c, return the largest.
+
+// intMax(1, 2, 3) → 3
+// intMax(1, 3, 2) → 3
+// intMax(3, 2, 1) → 3
+
+public int intMax(int a, int b, int c) {
+    if(a > b){
+      if(a > c){
+        return a;
+      }
+      else{
+        return c;
+      }
+    }
+    else{
+      if(b > c){
+        return b;
+      }
+      else{
+        return c;
+      }
+    }
+  }
+  
